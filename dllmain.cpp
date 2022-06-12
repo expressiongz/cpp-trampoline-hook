d390911ce1901f5243c58303ec6fc31e77174cc9
luxgz <97218884+luxgz@users.noreply.github.com>
2022-06-12 19:12:08 +0100

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -12,20 +12,20 @@ std::uint32_t tramp_hook( void* hook_addr, void* new_func, std::uint32_t instr_s
 
 	DWORD vp_old_prot;
 	VirtualProtect( hook_addr, instr_size, PAGE_EXECUTE_READWRITE, &vp_old_prot );
-	std::memset( hook_addr, 0x90, instr_size);
+	std::memset( hook_addr, 0x90, instr_size );
 
-	const auto rel_addr = ( reinterpret_cast< std::uint32_t>(new_func) - reinterpret_cast< std::uint32_t >(hook_addr) ) - jmp_instr_size;
+	const auto rel_addr = ( reinterpret_cast< std::uint32_t>( new_func ) - reinterpret_cast< std::uint32_t >( hook_addr ) ) - jmp_instr_size;
 
 	*( static_cast< std::uint8_t* >( hook_addr ) ) = 0xE9;
-	*( reinterpret_cast < std::uint32_t* >( reinterpret_cast< std::uint32_t>(hook_addr) + 1) ) = rel_addr;
+	*( reinterpret_cast < std::uint32_t* >( reinterpret_cast< std::uint32_t >( hook_addr ) + 1) ) = rel_addr;
 
 	VirtualProtect( reinterpret_cast< void* >( hook_addr ), instr_size, vp_old_prot, nullptr );
-	return reinterpret_cast< std::uint32_t>(hook_addr) + instr_size;
+	return reinterpret_cast< std::uint32_t >( hook_addr ) + instr_size;
 }
 
 std::uint32_t cont_execution_addr;
 
-void _declspec(naked) new_func() {
+void _declspec( naked ) new_func() {
 	__asm {
 		inc [ eax ]
 		lea eax, [ esp + 0x1C ]
